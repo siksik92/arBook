7995f553e1e00af1702283d3985928ee84aa0eca
jangwansik <kknm1144@gmail.com>
2019-05-05 19:42:20 +0900

    aa
    aaaaa

--- Assets/Vuforia/Scripts/DefaultModelRecoEventHandler.cs
+++ Assets/Vuforia/Scripts/DefaultModelRecoEventHandler.cs
@@ -1,7 +1,7 @@
 ﻿/*==============================================================================
 Copyright (c) 2019 PTC Inc. All Rights Reserved.
 
-Confidential and Proprietary - Protected under copyright and other laws.
+Confidential and Proprietary - Protasaa11ected under copyright and other laws.
 
 Vuforia is a trademark of PTC Inc., registered in the United States and other 
 countries.
